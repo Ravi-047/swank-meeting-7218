794641700a0471c620c39509bd416f1a1033d39d
dhiraj19999 <yogeshsamse682@gmail.com>
2022-08-25 23:04:00 +0530

    added world

--- topstory.js
+++ topstory.js
@@ -2,7 +2,7 @@ async function getdata(){
 
     try {
         
-    let res= await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=eb1cd6a6369142d08eacd7e381735525")
+    let res= await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=0c18b0d72b504269ada33e0c987a9eee")
     
     let data=  await res.json();
     let actualdata=data.articles;
@@ -32,7 +32,7 @@ async function getdata(){
     function appenddata(actualdata){
     
     for(let i=1;i<=4;i++){
-    if(i==3){
+    if(actualdata[i].urlToImage==null){
         continue;
     }
         let box= document.createElement("div");
@@ -42,7 +42,7 @@ async function getdata(){
     head.style.fontSize="25px"
     let image= document.createElement("img");
     image.src=actualdata[i].urlToImage;
-    image.style.width="500px"
+    image.style.width="400px"
     image.style.borderRadius="10px"
     let des=document.createElement("h4");
     des.innerText=actualdata[i].description;
@@ -61,7 +61,7 @@ async function getdata(){
     }
     
     for(let j=5;j<=22;j++){
-        if(j==13||j==15||j==18){
+        if(actualdata[j].urlToImage==null){
             continue;
         }
     
@@ -106,59 +106,56 @@ async function getdata(){
     
     async function infodata(){
     
-    try {
+        try {
+            
+        let res= await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=0c18b0d72b504269ada33e0c987a9eee&category=general")
         
-    let res= await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=eb1cd6a6369142d08eacd7e381735525&category=general")
-    
-    let data=  await res.json();
-    let realdata=data.articles;
-    if(realdata!==undefined){
-        third(realdata);
-    }
-    console.log(realdata);
-    
-    } catch (error) {
-       console.log(error);
-    }
-    
-    
-    
-    
-    
-    }
-    
-    infodata();
-    
-    
-    function third(realdata){
-    
-    for(let p=0;p<=10;p++){
-    
-        let box4= document.createElement("div");
-    let head4= document.createElement("h2");
-    head4.innerText=realdata[p].title;
-    head4.style.fontSize="15px";
-    head4.style.marginLeft="13px"
-    //head4.style.color="grey";
-    let image4= document.createElement("img");
-    image4.src=realdata[p].urlToImage;
-    image4.style.width="200px";
-    image4.style.borderRadius="10px"
-    
-    box4.append(head4,image4);
-    document.querySelector("#third").append(box4);
-    }
-    
-    //"ff"
-    
-    
-    }
-    
-    
-    // all ok///
-    
-    
-    
-    
-    
-    
+        let data=  await res.json();
+        let realdata=data.articles;
+        if(realdata!==undefined){
+            third(realdata);
+        }
+        console.log(realdata);
+        
+        } catch (error) {
+           console.log(error);
+        }
+        
+        
+        
+        
+        
+        }
+        
+        infodata();
+        
+        
+        function third(realdata){
+        
+        for(let p=0;p<=10;p++){
+        if(realdata[p].urlToImage==null){
+            continue;
+        }
+            let box4= document.createElement("div");
+        let head4= document.createElement("h2");
+        head4.innerText=realdata[p].title;
+        head4.style.fontSize="15px";
+        head4.style.marginLeft="13px"
+        //head4.style.color="grey";
+        let image4= document.createElement("img");
+        image4.src=realdata[p].urlToImage;
+        image4.style.width="200px";
+        image4.style.borderRadius="10px"
+        
+        box4.append(head4,image4);
+        document.querySelector("#third").append(box4);
+        }
+        
+        //"ff"
+        
+        
+        }
+        
+        
+
+
