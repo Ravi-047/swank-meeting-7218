4b3a8d901eecd14cdea1afac9188e91b3cbf9ccb
Ravi-047 <ravi047raj@gmail.com>
2022-08-26 18:40:42 +0530

    import footer

---
+++ Script/footer.js
@@ -0,0 +1,9 @@
+console.log("Working footer.js");
+
+import footer from "../components/fOOter.js";
+
+
+let footer_container = document.querySelector("footer");
+footer_container.innerHTML = footer();
+
+console.log(footer_container);
