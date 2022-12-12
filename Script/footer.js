f01f7ec75e49afaf2e05d8be44c3c025be1697af
dhiraj garad <yogeshsamse682@gmail.com>
2022-12-13 00:09:53 +0530

    add

--- Script/footer.js
+++ Script/footer.js
@@ -1,7 +1,7 @@
 console.log("Working footer.js");
 
 import footer from "../components/fOOter.js";
-
+//
 
 let footer_container = document.querySelector("footer");
 footer_container.innerHTML = footer();
